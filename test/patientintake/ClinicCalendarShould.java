1b088b603757697b245f9b1e78082dedade34597
Nitin Jaswal <nitin.jaswal@gmail.com>
2020-09-05 19:17:33 +0530

    Finito

--- test/patientintake/ClinicCalendarShould.java
+++ test/patientintake/ClinicCalendarShould.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
@@ -13,9 +14,17 @@ class ClinicCalendarShould {
 
    private ClinicCalendar calendar;
 
+   @BeforeAll
+   static void intiAll() {
+      System.out.println("Before all..");
+   }
+
+   // Usually you just need only this
    @BeforeEach
    void init() {
+      System.out.println("Before each..");
       calendar = new ClinicCalendar(LocalDate.of(2018, 8, 26));
+//      calendar = new ClinicCalendar(LocalDate.now()); // will fail, so above is better
    }
 
    @Test
@@ -27,10 +36,30 @@ class ClinicCalendarShould {
       assertEquals(1, appointments.size());
       PatientAppointment enteredAppt = appointments.get(0);
 
+      // Assertions short circuit- so you won't see the second failure along with the first. Fix first then run into
+      // next. Fix that and so on.
+      // It would have saved us some time if we saw all the mismatches on the appointment data at one go
+      // Useful if several assertions are really checking just the same thing
+      // 4 assertions but really just 1 assumption. Created appointment matches one entered by user
+      // A series of assertions in one test method use assertAll- prevents short circuiting of any assertions it wraps
+      // each of the parameters needs to be a lambda expression
+
+      /*
+      assertEquals("Jim", enteredAppt.getPatientFirstName());
+      assertEquals("Weaver", enteredAppt.getPatientLastName());
+      assertEquals(Doctor.avery, enteredAppt.getDoctor());
+      assertEquals("9/1/2018 02:00 PM",
+              enteredAppt.getAppointmentDateTime().format(DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a", Locale.US)));
+      */
+
       assertAll(
          () -> assertEquals("Jim", enteredAppt.getPatientFirstName()),
          () -> assertEquals("Weaver", enteredAppt.getPatientLastName()),
-         () -> assertSame(Doctor.avery, enteredAppt.getDoctor()),
+         () -> assertSame(Doctor.avery, enteredAppt.getDoctor()), // will verify that the 2 objects being compared
+              // point to literally the same object in memory- enum instances are unique so these two shouldn't
+              // be just equivalent they should be EXACTLY the same object in memory
+              // use assertNotSame to verify that an object that's supposed to have been copied to another object
+              // isn't the same instance as the original it was copied from
          () -> assertEquals("9/1/2018 02:00 PM",
             enteredAppt.getAppointmentDateTime().format(DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a", Locale.US)))
       );
@@ -49,6 +78,7 @@ class ClinicCalendarShould {
    }
 
    @Test
+//   @Disabled // lets you skip these tests, but still keep their presence visible in the report
    void returnCurrentDaysAppointments() {
       calendar.addAppointment("Jim", "Weaver", "avery",
          "08/26/2018 2:00 pm");
@@ -57,5 +87,31 @@ class ClinicCalendarShould {
       calendar.addAppointment("Jim", "Weaver", "avery",
          "09/01/2018 2:00 pm");
       assertEquals(2, calendar.getTodayAppointments().size());
+//      assertEquals(calendar.getTodayAppointments(), calendar.getAppointments()); // compares object instance references
+      // for collections jUnit will check the two collections contain equivalent objects using equals() and that they are
+      // in the same order
+
+      // Collection specific assertion
+//      assertIterableEquals(calendar.getTodayAppointments(), calendar.getAppointments());
+      // An iterable in java is something that returns a series of objects for use in a for loop
+      // If you are trying to compare contents of different collection types, if you can get an iterable from both of
+      // them, you can use Junit5s assertIterable to compare them
+   }
+
+   @Test
+   void testStringSplitLimit() {
+      String[] arr = {"b", "", ":and:f", "", "" };
+      assertIterableEquals(Arrays.asList(arr), Arrays.asList("boo:and:foo".split("o", 5)));
+   }
+
+   // to clean up test data, even if an unexpected exception occurs during a test
+   @AfterEach
+   void tearDown() {
+      System.out.println("After each..");
+   }
+
+   @AfterAll
+   static void tearDownAll() {
+      System.out.println("After all..");
    }
 }
--- test/patientintake/DateTimeConverterShould.java
+++ test/patientintake/DateTimeConverterShould.java
@@ -8,8 +8,11 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@Tag("dateTime") // I could scatter tags across multiple test classes & the test runner will only target those
+// tests that match the tag expression
 @DisplayName("DateTimeConverter should")
 class DateTimeConverterShould {
 
@@ -23,6 +26,16 @@ class DateTimeConverterShould {
          LocalDateTime result = DateTimeConverter.convertStringToDateTime("today 1:00 pm",
             today);
          assertEquals(result, LocalDateTime.of(2018, 9, 1, 13, 0),
+            // see what date was passed for the today parameter on failure
+                 // useful for test failures that are difficult to understand
+                 // if you have many such test methods calculating the failure message string is taking
+                 // some time and slowing the test down- as it stands this string will be computed
+                 // even if tests pass. We can use a slightly different syntax- using a lambda expression
+                 // for the 3rd parameter (anonymous function basically) to provide the failure message
+                 // instead of just a fixed string - same results on a failure but the lambda expression will
+                 // only be evaluated if there IS a failure. Speed up test execution!
+
+//            "Failed to convert 'today' string to expected date time, today passed was: " +today);
             () -> "Failed to convert 'today' string to expected date time, today passed was: " +today);
       }
 
@@ -46,11 +59,14 @@ class DateTimeConverterShould {
    }
 
    @Test
+//   @Tag("dateTime")
    @DisplayName("throw exception if entered pattern of string incorrect")
    void throwExceptionIfIncorrectPatternProvided() {
+      // Junit5 assertion that we can use to explicitly verify that we get an exception when certain code is called
       Throwable error = assertThrows(RuntimeException.class, () ->
          DateTimeConverter.convertStringToDateTime("9/2/2018 100 pm",
          LocalDate.of(2018, 9, 1)));
+      // verify the text/message of the exception is what we expect it to be
       assertEquals("Unable to create date time from: [9/2/2018 100 pm], " +
          "please enter with format [M/d/yyyy h:mm a], Text '9/2/2018 100 PM' " +
          "could not be parsed at index 12", error.getMessage());
